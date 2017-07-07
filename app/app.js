08015a9d3d8c139f2b1448c96416434f25fdb49c
cookies4u <jcreyes.1018@gmail.com>
2017-07-06 20:58:32 -0700

    api route working. not working with DB

--- app/app.js
+++ app/app.js
@@ -5,5 +5,6 @@ var ReactDOM = require("react-dom");
 // Include the main Main Component
 var Main = require("./components/Main");
 
+
 // This code here allows us to render our main component (in this case Main)
 ReactDOM.render(<Main />, document.getElementById("app"));
